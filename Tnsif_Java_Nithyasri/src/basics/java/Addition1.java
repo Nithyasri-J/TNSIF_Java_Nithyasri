9df6c38f1cd0d33bd8241f9ed3ce7f12e6063a71
nithyasri <rakshanasri1156@gmail.com>
2025-09-30 09:47:03 +0530

    commit 2

---
+++ Tnsif_Java_Nithyasri/src/basics/java/Addition1.java
@@ -0,0 +1,11 @@
+package basics.java;
+
+public class Addition1 {
+   public static void main(String args[]) {
+	// TODO Auto-generated method stub
+			int a=5;
+			int b=10;
+			System.out.print(a+b);
+
+   }
+}
