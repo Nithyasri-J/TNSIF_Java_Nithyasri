0c04f8f715766a448d4d4d45701b9e9cd4834dae
nithyasri <rakshanasri1156@gmail.com>
2025-10-08 14:09:38 +0530

    commit 7

---
+++ Tnsif_Java_Nithyasri/src/Day_5/GreetingDemo.java
@@ -0,0 +1,11 @@
+package Day_5;
+
+public class GreetingDemo {
+	//Lambda Expression
+			GreetingDemo gd = () -> 
+			System.out.println("Hello Everyone, Welcome to the Java Session");
+			
+			gd.greet();
+			
+
+}
