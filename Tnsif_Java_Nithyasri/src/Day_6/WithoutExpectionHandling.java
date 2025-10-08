82a6205bcdb54a1c67e7c70a4e5f9c94267a495d
nithyasri <rakshanasri1156@gmail.com>
2025-10-08 23:17:27 +0530

    commit day6

---
+++ Tnsif_Java_Nithyasri/src/Day_6/WithoutExpectionHandling.java
@@ -0,0 +1,12 @@
+package Day_6;
+
+public class WithoutExpectionHandling {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("the program continues....");
+		int data=100/0;
+
+	}
+
+}
